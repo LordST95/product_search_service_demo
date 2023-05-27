2c4a979fcac95f1ce20353a30c38d10db19487ae
Sina T <sina.tamjidy@gmail.com>
2023-05-27 12:13:07 +0330

    refactore - rename endpoint of CartCreateUpdateView

--- src/api/urls.py
+++ src/api/urls.py
@@ -14,10 +14,8 @@ urlpatterns = [
     # Orders
     path('orders/all/', views.CartListView.as_view(),
          name="all_carts"),
-    path('orders/add_to_unpaid_cart/', views.CartCreateUpdateView.as_view(),
-         name="add_to_unpaid_cart"),
-    # path('orders/remove_from_unpaid_cart/', views.CartUpdateView.as_view(),
-    #      name="remove_from_unpaid_cart"),
+    path('orders/update_unpaid_cart/', views.CartCreateUpdateView.as_view(),
+         name="update_unpaid_cart"),
     # path('orders/mark_cart_as_paid/<int:pk>/', views.CartMarkPaidView.as_view(),
     #      name="mark_cart_as_paid"),
     # path('orders/detail/<int:pk>/', views.CartDetailView.as_view(),
