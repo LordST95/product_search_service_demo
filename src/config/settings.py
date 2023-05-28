32a9a4fe797c226fdd4b6398b35867af20e2300e
Sina T <sina.tamjidy@gmail.com>
2023-05-29 00:04:17 +0330

    change db to postgres

--- src/config/settings.py
+++ src/config/settings.py
@@ -82,18 +82,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # # 'default': {
+    # #     'ENGINE': 'django.db.backends.sqlite3',
+    # #     'NAME': BASE_DIR / 'db.sqlite3',
+    # # }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'product_search_db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'fake_pass',
+    #     'HOST': 'db',
+    #     'PORT': '3306',
+    #     'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'product_search_db',
-        'USER': 'root',
-        'PASSWORD': 'fake_pass',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'db',
-        'PORT': '3306',
-        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+        'PORT': '5433',
     }
 }
 
