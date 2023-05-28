45c53203bc956a4087b6390120a97f23607ba174
Sina T <sina.tamjidy@gmail.com>
2023-05-28 15:55:18 +0330

    change type of email sending running to apply_async based on https://stackoverflow.com/a/48910727/4417806

--- src/accounts/signals.py
+++ src/accounts/signals.py
@@ -9,17 +9,10 @@ from accounts.tasks import send_welcome_email
 
 @receiver(post_save, sender=Member, dispatch_uid="send_email_for_new_users")
 def send_email(sender, instance=None, created=False, **kwargs):
+    # TODO, apply_async doesn't work on my windows, check if it is ok in linux based server
     if created:
-        today = datetime.now()
-        # sending_time = today + timedelta(seconds=5, days=1)
-        sending_time = today + timedelta(seconds=10)
-        print(f"{sending_time= }")
-        # method1 ==> task does not start in the windows :|
-        # result = send_welcome_email.apply_async(args=(instance.username,), eta=sending_time)
-        # # .get() forced it to wait
-        # result_output = result.get()
-        # method2
+        # sending_time = datetime.now() + timedelta(seconds=5, days=1)
+        sending_time = datetime.now() + timedelta(seconds=10)
         if instance.email:
-            send_welcome_email.delay(instance.username, instance.email)
-        
+            send_welcome_email.apply_async(args=(instance.username,), eta=sending_time)
     return "sth"
